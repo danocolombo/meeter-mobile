a59ec0cd72ca5fa70f5f0833942abb09ca51514c
Dano Colombo <danocolombo@gmail.com>
2022-04-11 17:17:15 -0400

    basics wired up

---
+++ screens/HistoricScreen.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState, useContext } from 'react';
+import { AuthContext } from '../store/auth-context';
+import { StyleSheet, Text, View } from 'react-native';
+import MeetingCard from '../components/Meeting/MeetingCard';
+
+function HistoricScreen() {
+    const [fetchedMessage, setFetchedMessage] = useState();
+    const authCtx = useContext(AuthContext);
+    const token = authCtx.token;
+    useEffect(() => {
+        axios
+            .get(
+                'https://react-native-max-2022-default-rtdb.firebaseio.com/message.json?auth=' +
+                    token
+            )
+            .then((response) => {
+                setFetchedMessage(response.data);
+            });
+    }, [token]);
+    return (
+        <View style={styles.rootContainer}>
+            <MeetingCard />
+            <Text style={styles.title}>Welcome!</Text>
+            <Text>You authenticated successfully!</Text>
+            <Text>{fetchedMessage}</Text>
+        </View>
+    );
+}
+
+export default HistoricScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        // justifyContent: 'center',
+        alignItems: 'center',
+        padding: 32,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+});
