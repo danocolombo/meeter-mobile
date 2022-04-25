8e94782676558092919f5bfd2b5bfe42bec665ff
Dano Colombo <danocolombo@gmail.com>
2022-04-25 14:44:02 -0400

    updated auth to ue .env

--- util/auth.js
+++ util/auth.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
-const API_KEY = 'AIzaSyBAQLQgkVBlJ8XN3GnULmBJWY0C124ubeE';
+import { GOOGLE_AUTH, GOOGLE_API_KEY } from '@env';
 
 async function authenticate(mode, email, password) {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    const url = `${GOOGLE_AUTH}:${mode}?key=${GOOGLE_API_KEY}`;
 
     const response = await axios.post(url, {
         email: email,
