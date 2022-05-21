20844d53a621e8e97232d0b600a193c981a36a5a
Dano Colombo <danocolombo@gmail.com>
2022-05-21 07:12:41 -0400

    user redux loaded from cognito

--- features/meetings/userSlice.js
+++ features/meetings/userSlice.js
@@ -1,14 +1,33 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { printObject } from '../../util/helpers';
 
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        authToken: null,
-        userName: null,
+        authToken: '',
+        userId: '',
+        userName: '',
+        firstName: '',
+        lastName: '',
+        gender: '',
+        email: '',
     },
     reducers: {
         loadUser: (state, action) => {
+            // console.log('*******************************');
+            // printObject('userSlice - action', action);
+            // console.log('*******************************');
+            state.authToken = action.payload.token;
+            state.userId = action.payload.uid;
+            state.userName = action.payload.userName;
+            state.firstName = action.payload.firstName;
+            state.lastName = action.payload.lastName;
+            state.gender = action.payload.gender;
+            state.email = action.payload.email;
+            return state;
+        },
+        loadUser1: (state, action) => {
             state.authToken = action.payload.token;
             state.userName = action.payload.userName;
         },
--- screens/ActiveScreen.js
+++ screens/ActiveScreen.js
@@ -10,18 +10,37 @@ import { fetchActiveMeetings } from '../providers/meetings';
 import { saveActiveMeetings } from '../features/meetings/meetingsSlice';
 import { DatePickerAndroid, StyleSheet, Text, View } from 'react-native';
 import { printObject } from '../util/helpers';
-
+import { Auth } from 'aws-amplify';
+import { loadUser } from '../features/meetings/userSlice';
 function ActiveScreen() {
     const dispatch = useDispatch();
     const [isLoading1, setIsLoading] = useState(false);
     const isLoading = useSelector((state) => state.meetings.isLoading);
     const authToken = useSelector((state) => state.user.authToken);
     let activeMeetings = useSelector((state) => state.meetings.activeMeetings);
+
     useEffect(() => {
         // if (authToken.length === null && activeMeetings.length === 0) {
         // dispatch(loadUser({ token: token, userName: email }));
         dispatch(getActiveMeetings(), null);
         // }
+        Auth.currentSession()
+            .then((data) => {
+                //console.log(data);
+                let user = {
+                    uid: data.idToken.payload.sub,
+                    userName: data.accessToken.payload.username,
+                    token: data.idToken.jwtToken,
+                    gender: data.idToken.payload.gender,
+                    firstName: data.idToken.payload.given_name,
+                    lastName: data.idToken.payload.family_name,
+                    email: data.idToken.payload.email,
+                };
+                dispatch(loadUser(user));
+                // printObject('user', user);
+                // console.log('target:', data.idToken.payload.family_name);
+            })
+            .catch((err) => console.log(err));
     }, []);
     // printObject('activeMeetings', activeMeetings);
     if (activeMeetings.length < 1) {
@@ -32,9 +51,16 @@ function ActiveScreen() {
         );
     }
     // console.log('activeMeetings.length', activeMeetings.length);
-    return isLoading ? (
-        <LoadingOverlay />
-    ) : (
+    // return isLoading ? (
+    //     <LoadingOverlay />
+    // ) : (
+    //     <View style={styles.rootContainer}>
+    //         <NextMeetingCard nextMeeting={activeMeetings[0]} />
+    //         <Text style={styles.title}>Welcome!</Text>
+    //         <MeetingsOutput meetings={activeMeetings} />
+    //     </View>
+    // );
+    return (
         <View style={styles.rootContainer}>
             <NextMeetingCard nextMeeting={activeMeetings[0]} />
             <Text style={styles.title}>Welcome!</Text>
